ddfd4f9559b8f8d274dbca051c131e395b0fae1f
Eva Marques <eva0marques@gmail.com>
2023-09-27 14:39:18 -0400

    Create test-output-class.R

---
+++ tests/testthat/test-output-class.R
@@ -0,0 +1,12 @@
+#' @author Ranadeep Daw and Eva Marques
+#' @description
+#' unit testing: is the model output netcdf?
+#' 
+#'
+
+test_that("the model output is netcdf", {
+  library(ncdf4)
+
+  # the test is running on the object named "output"
+  expect_is(output, 'ncdf4')
+})
