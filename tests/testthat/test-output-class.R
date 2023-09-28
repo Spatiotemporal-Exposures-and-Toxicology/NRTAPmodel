59878cc57a24e0633eba626722f7195e40e48382
Eva Marques <eva0marques@gmail.com>
2023-09-28 11:47:58 -0400

    Update test-output-class.R

--- tests/testthat/test-output-class.R
+++ tests/testthat/test-output-class.R
@@ -8,5 +8,5 @@ test_that("the model output is netcdf", {
   library(ncdf4)
 
   # the test is running on the object named "output"
-  expect_is(output, 'ncdf4')
+  expect_is(model.output, 'ncdf4')
 })
