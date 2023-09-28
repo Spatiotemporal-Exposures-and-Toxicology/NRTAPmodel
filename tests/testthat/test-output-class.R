710ab374737a3bb1efb5d8e55d0d295ef6c22ebb
Eva Marques <eva0marques@gmail.com>
2023-09-28 15:48:11 -0400

    Global to local call of library in testthat

--- tests/testthat/test-output-class.R
+++ tests/testthat/test-output-class.R
@@ -5,7 +5,7 @@
 #'
 
 test_that("the model output is netcdf", {
-  library(ncdf4)
+  withr::local_package("ncdf4")
 
   # the test is running on the object named "output"
   expect_is(model.output, 'ncdf4')
